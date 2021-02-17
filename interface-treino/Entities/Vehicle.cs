f30514b99e55a572305941c3573c9bb8c2e6f773
Raphael Scursoni <ra.scursoni@gmail.com>
2021-02-17 11:07:45 -0300

    Projeto sem interface para entender como ser melhorado

---
+++ interface-treino/Entities/Vehicle.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace interface_treino.Entities {
+    class Vehicle {
+        public string Model { get; set; }
+
+        public Vehicle(string model)
+        {
+            Model = model;
+        }
+    }
+}
